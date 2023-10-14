53981552e15c3dc3f2d71009caefc0632b5ebaeb
SAURAV KUMAR <126794077+Sauravkrgupta@users.noreply.github.com>
2023-10-14 21:48:36 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css'; // Add this line
+import { Container } from './components/Container';
+
+function App() {
+  return (
+    <Container />
+  );
+}
+
+export default App;
